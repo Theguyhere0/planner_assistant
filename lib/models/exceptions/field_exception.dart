e4401dc4d9a4c1a6318e70133602a733d8a2ec94
Theguyhere <mxlin01@gmail.com>
2022-11-14 19:47:25 -0500

    Built web

--- lib/models/exceptions/field_exception.dart
+++
@@ -1,7 +0,0 @@
-/// An exception thrown when something goes wrong related to a [Field] object.
-class FieldException implements Exception {
-  const FieldException(this.cause);
-
-  /// The reason for this [Exception].
-  final String cause;
-}
--- lib/utils/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-const primaryColor = Color(0xff0088ff);
-const secondaryColor = Color(0xff2f3357);
-const bgColor = Color(0xff1a1e3d);
-
-/// 16.0
-const defaultPadding = 16.0;
